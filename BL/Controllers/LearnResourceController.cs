0272ac83ba4a794066d95fcbb57e6d7d15eafafe
kry.ban <kry.ban@gmail.com>
2021-08-28 00:03:29 +0200

    Initial commit on BlijvenLeren project

---
+++ BL/Controllers/LearnResourceController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BL.Data;
+using BL.Models;
+
+namespace BL.Controllers
+{
+    public class LearnResourceController : Controller
+    {
+        private readonly BLContext _context;
+
+        public LearnResourceController(BLContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LearnResource
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LearnResourceModel.ToListAsync());
+        }
+
+        // GET: LearnResource/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var learnResourceModel = await _context.LearnResourceModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (learnResourceModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(learnResourceModel);
+        }
+
+        // GET: LearnResource/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LearnResource/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Link")] LearnResourceModel learnResourceModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(learnResourceModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(learnResourceModel);
+        }
+
+        // GET: LearnResource/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var learnResourceModel = await _context.LearnResourceModel.FindAsync(id);
+            if (learnResourceModel == null)
+            {
+                return NotFound();
+            }
+            return View(learnResourceModel);
+        }
+
+        // POST: LearnResource/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Link")] LearnResourceModel learnResourceModel)
+        {
+            if (id != learnResourceModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(learnResourceModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LearnResourceModelExists(learnResourceModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(learnResourceModel);
+        }
+
+        // GET: LearnResource/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var learnResourceModel = await _context.LearnResourceModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (learnResourceModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(learnResourceModel);
+        }
+
+        // POST: LearnResource/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var learnResourceModel = await _context.LearnResourceModel.FindAsync(id);
+            _context.LearnResourceModel.Remove(learnResourceModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LearnResourceModelExists(int id)
+        {
+            return _context.LearnResourceModel.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BL/Models/LearnResourceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BL.Models
+{
+    public class LearnResourceModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
