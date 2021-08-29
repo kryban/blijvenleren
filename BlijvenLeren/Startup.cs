f12f75749875f58ceb2dcd51f1633ebdc5f929e2
kry.ban <kry.ban@gmail.com>
2021-08-29 11:15:53 +0200

    trigger new workflow

--- BlijvenLeren/Startup.cs
+++ BlijvenLeren/Startup.cs
@@ -1,15 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using BlijvenLeren.Data;
+using BlijvenLeren.Repository;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using BlijvenLeren.Data;
-using BlijvenLeren.Repository;
 
 namespace BlijvenLeren
 {
