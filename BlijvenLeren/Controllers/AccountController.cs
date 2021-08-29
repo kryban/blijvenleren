e0b80b847df8507084e79be54416ef9e1bd48345
kry.ban <kry.ban@gmail.com>
2021-08-29 19:31:00 +0200

    initial role manager

--- BlijvenLeren/Controllers/AccountController.cs
+++ BlijvenLeren/Controllers/AccountController.cs
@@ -13,12 +13,14 @@ namespace BlijvenLeren.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(UserManager<IdentityUser> userManager,
-                                      SignInManager<IdentityUser> signInManager)
+        public AccountController(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
 
